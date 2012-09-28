a3bac6caefba7bf49c1a3111b45f68a0d1e00a34
Karthik Ram <karthik.ram@gmail.com>
2012-09-27 23:02:59 -0700

    modified print method

--- R/metrics_methods.R
+++ R/metrics_methods.R
@@ -1,3 +1,13 @@
+#' Internal function to match provider names
+#'
+#' @param provider provider name
+#' @keywords internal
+return_prov <- function(provider) {
+	services <- data.frame(type = c("cited_by_gplus_count", "cited_by_fbwalls_count", "cited_by_posts_count", "cited_by_tweeters_count", "cited_by_accounts_count", "cited_by_feeds_count", "cited_by_rdts_count", "cited_by_msm_count", "cited_by_delicious_count", "cited_by_forum_count", "cited_by_qs_count"), names = c("Google+", "Facebook", "Cited", "Tweets", "Accounts", "Feeds", "Reddit", "MSM", "Delicious", "Forums", "QS"))
+
+	return(services$names[which(services$type == provider)])
+}
+
 #' Print a summary for an altmetric object
 #' @method print altmetric
 #' @param alt_obj An object of class \code{Altmetric}
@@ -5,6 +15,11 @@ print.altmetric <- function(alt_obj) {
 	string <- "Altmetrics on: \"%s\" with doi %s (altmetric_id: %s) published in %s."
     vals   <- c(alt_obj$title, alt_obj$doi, alt_obj$altmetric_id, alt_obj$journal)
     cat(do.call(sprintf, as.list(c(string, vals))))
+    cat("\n")
+    stats <- melt(alt_obj[grep("^cited", names(alt_obj))])
+stats$names <- unname(sapply(stats$L1, return_prov))
+stats$names <- factor(stats$names, levels = stats$names[rev(order(stats$value))])
+ print(stats)
 }
 
 
@@ -14,13 +29,6 @@ print.altmetric <- function(alt_obj) {
 #' @method plot altmetric
 #' @param alt_obj An object of class \code{Altmetric}
 plot.altmetric <- function(alt_obj) {
-
-services <- data.frame(type = c("cited_by_gplus_count", "cited_by_fbwalls_count", "cited_by_posts_count", "cited_by_tweeters_count", "cited_by_accounts_count", "cited_by_feeds_count", "cited_by_rdts_count", "cited_by_msm_count", "cited_by_delicious_count", "cited_by_forum_count", "cited_by_qs_count"), names = c("Google+", "Facebook", "Cited", "Tweets", "Accounts", "Feeds", "Reddit", "MSM", "Delicious", "Forums", "QS"))
-
-return_prov <- function(foo) {
-	return(services$names[which(services$type == foo)])
-}
-
 stats <- melt(alt_obj[grep("^cited", names(alt_obj))])
 stats$names <- unname(sapply(stats$L1, return_prov))
 stats$names <- factor(stats$names, levels = stats$names[rev(order(stats$value))])
@@ -33,3 +41,4 @@ ggplot(stats, aes(names, value)) + geom_point(size = 4, colour = 'steelblue') + 
         panel.grid.minor = element_blank(), panel.border = element_blank(),
         axis.line = element_line(colour = "black")) + annotation_raster(donut, xmin = dim(stats)[1]-1, xmax = dim(stats)[1], ymin = max(stats$value)-60, ymax = max(stats$value), interpolate = T) + theme(title = element_text(family = "Helvetica", colour = "#0680b0", face="bold"), axis.text = element_text(family="Courier", colour = "#3f3f3f"), axis.title = element_text(colour="#3f3f3f")) 
 }
+
